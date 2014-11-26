26a640c366667f59aeecbe23488e02976c1f7e15
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-11-26 21:24:24 +0200

    Update OpenStatus.py

--- OpenStatus.py
+++ OpenStatus.py
@@ -1,12 +1,23 @@
 'OpenStatus - Backend to the OpenStatus decentralized social network.'
 'GNU GPL v3.0 '
 
+class user():
+    name_raw = open("homedir/.OpenStatus/config/name", "r")
+    name = name_raw.read()
+    name_raw.close()
+    def current_time():
+        return datetime
+
+class generator():
+    def generate_post(content):
+        return "[" + user.current_time() + "||" + user.name + "] " + content
+
 class timeline():
     def retrieve():
         return open("/var/OpenStatus/serve/stream.txt", "r").readlines()
     
     def post(content):
-        writer = open("/var/OpenStatus/serve/stream.txt", "a").write(content)
+        writer = open("/var/OpenStatus/serve/stream.txt", "a").write(generator.generate_post(content))
         writer.close()
     
     def 
