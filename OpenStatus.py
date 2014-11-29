67a281f7989d0835c8116ca4385099afdeaf33ef
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-11-29 18:06:06 +0200

    Such update.

--- OpenStatus.py
+++ OpenStatus.py
@@ -5,12 +5,14 @@ import datetime
 import os
 
 class user():
+    'Get the currently logged in user"s OpenStatus name.'
     name = open(os.environ("HOME") + "/.OpenStatus/config.txt", "r").readline(0).strip("name=")
     def current_time():
         return str(datetime.time.hour) + ":" + str(datetime.time.minute) + ":" + str(datetime.time.second)
 
 class timestamper():
     def stamp():
+        'Create a timestapmp and return it to the user.'
         return "[" + user.current_time() + "||" + user.name + "] "
 
 class timeline():
